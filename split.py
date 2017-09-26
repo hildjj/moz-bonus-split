43d36aa5ce7f4ad096831d502b192f484f70c8b4
Joe Hildebrand <joe-github@cursive.net>
2017-09-26 10:26:50 -0600

    Make work for September bonus letters

--- split.py
+++ split.py
@@ -10,15 +10,21 @@ parser.add_argument("-s", "--secret", help="Secret with which to encrypt each pa
 args = parser.parse_args()
 
 pdf = PyPDF2.PdfFileReader(args.PDFfile)
+pn = 0
 for page in pdf.pages:
   d = page.getContents().getData()
+  pn += 1
   # Horrible hack.  Close enough for now and seems to work.
-  m = re.search('Tm\n\(([^)]+):\)Tj\n', d)
-  if (m):
+  m = re.findall(b'Tm\n\(([^)]+)\)Tj\n', d)
+  try:
+    colon = m.index(b':')
+    name = m[colon-1]
     out = PyPDF2.PdfFileWriter()
     out.addPage(page)
     if args.secret:
       out.encrypt(args.secret)
-    of = open(m.group(1) + ".pdf", "wb")
+    of = open(name + b".pdf", "wb")
     out.write(of)
     of.close()
+  except ValueError:
+    print("Name not found on page", pn)
